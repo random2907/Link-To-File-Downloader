37fa0779e6076536947658578c8975da6f6f0063
Random2907 <yranjeetyadav1234567890@gmail.com>
2022-02-14 22:06:54 +0530

    added more link support

--- link_to_file_downloader.py
+++ link_to_file_downloader.py
@@ -1,3 +1,5 @@
+from urllib.error import HTTPError
+import requests
 import yt_dlp
 import subprocess
 import wget
@@ -8,25 +10,30 @@ for k1 in range (num):
     url =input("enter url = ")
     x.append(url)
 n=0    
-for i in range(num):
-    z=x[n]
-    z=z.strip()
-    y=z.split('/')
-    if y[2]=="drive.google.com":
-        print('drive link : ',z)
-        gdown.download(z,fuzzy=True)
-        n+=1
-    elif y[2]=='www.youtube.com' or y[2]=='youtu.be':
-        print('youtube link : ',z) 
-        ydl_opts = {}
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([z])
-        n+=1
-    elif y[2]!='www.youtube.com' or y[2]!='youtu.be' and y[2]!='drive.google.com':
-        print('direct link :',z)
-        wget.download(z)
-        n+=1    
-     
+for i in range(num):      
+    try:
+        z=x[n]
+        z=z.strip()
+        y=z.split('/')
+        if y[2]=="drive.google.com":
+            print('drive link : ',z)
+            gdown.download(z,fuzzy=True)
+            n+=1
+        elif y[2]=='www.youtube.com' or y[2]=='youtu.be':
+            print('youtube link : ',z) 
+            ydl_opts = {}
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([z])
+            n+=1
+        elif y[2]!='www.youtube.com' or y[2]!='youtu.be' and y[2]!='drive.google.com':
+            print('direct link :',z)
+            wget.download(z)
+            n+=1    
+    except HTTPError:
+        r = requests.get(z, allow_redirects=True)
+        k=z.split('/')
+        open(k[-1], 'wb').write(r.content)
+         
 
 
 
